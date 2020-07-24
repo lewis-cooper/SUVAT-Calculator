b31cc26cccf4a56c1a3b9418442cd64e91ce9296
Lewis <62891818+lewis-cooper@users.noreply.github.com>
2020-07-24 23:31:56 +0100

    First full release of GUI SUVAT Calculator
    First polished release of the GUI calculator. Still a few bug fixes and efficiency problems to fix but on the whole, this is now a fully working model of the GUI SUVAT calculator

--- suvat_calculator_GUI.py
+++ suvat_calculator_GUI.py
@@ -1,15 +1,15 @@
 import tkinter as tk
 import math
-import time
 
-HEIGHT = 700			             #<-- Changes the height of the window
-WIDTH = 550				             #<-- Changes the height of the window
+HEIGHT = 700			  #<-- Changes the height of the window
+WIDTH = 550				  #<-- Changes the height of the window
 
 #Colours
 PrussianBlue = '#1D3557'
 PowderBlue = '#A8DADC'
 ImperialRed = '#E63946'
 Honeydew = '#F1FAEE'
+Xiketic = '#04030F'
 
 
 #Starting variables with blank values
@@ -32,16 +32,37 @@ t = None
  / __/ / /_/ / /|  / /___  / / _/ // /_/ / /|  /___/ /
 /_/    \____/_/ |_/\____/ /_/ /___/\____/_/ |_//____/
 
-'''
 
+'''
+#ERRORS
 #Creating error message for when input is not a number or blank
-def errorMsg():
+def ValueErrorMsg():
     global error
     error = tk.Label(mainframe, text="ERROR: Invalid Input. Enter only a number or leave blank", fg=ImperialRed, bg=PrussianBlue)
     error.place(relx=0, rely=0.7, relheight=0.1, relwidth=1)
     error.config(font=("Helvetica", 12))
 
+def InputErrorMessage():
+    global inputerror
+    inputerror = tk.Label(mainframe, text="ERROR: Invalid Input. Enter at least 3 variables", fg=ImperialRed, bg=PrussianBlue)
+    inputerror.place(relx=0, rely=0.7, relheight=0.1, relwidth=1)
+    inputerror.config(font=("Helvetica", 12))
 
+
+#functions for the clear button to remove all text from entry boxes
+def clear():
+    s_val.delete(0,"end")
+    u_val.delete(0,"end")
+    v_val.delete(0,"end")
+    a_val.delete(0,"end")
+    t_val.delete(0,"end")
+
+
+def errorclear():
+    global error
+    global inputerror
+    error.destroy()
+    inputerror.destroy()
 
 
 #Storing the values for SUVAT as either a float or a blank
@@ -53,7 +74,7 @@ def s_value():
 			if s_val.get() == "":
 				break
 			else:
-				return errorMsg()
+				return ValueErrorMsg()
 
 
 def u_value():
@@ -64,7 +85,7 @@ def u_value():
 			if u_val.get() == "":
 				break
 			else:
-				return errorMsg()
+				return ValueErrorMsg()
 
 
 def v_value():
@@ -75,7 +96,7 @@ def v_value():
 			if v_val.get() == "":
 				break
 			else:
-				return errorMsg()
+				return ValueErrorMsg()
 
 
 def a_value():
@@ -86,7 +107,7 @@ def a_value():
 			if a_val.get() == "":
 				break
 			else:
-				return errorMsg()
+				return ValueErrorMsg()
 
 
 def t_value():
@@ -97,19 +118,9 @@ def t_value():
 			if t_val.get() == "":
 				break
 			else:
-				return errorMsg()
+				return ValueErrorMsg()
 
 
-
-#function for the clear button to remove all text from entry boxes
-def clear():
-    global error
-    s_val.delete(0,"end")
-    u_val.delete(0,"end")
-    v_val.delete(0,"end")
-    a_val.delete(0,"end")
-    t_val.delete(0,"end")
-    error.destroy()
 
 '''
    ________  ______
@@ -188,7 +199,9 @@ t_val = tk.Entry(mainframe, fg=Honeydew, bg=PowderBlue)
 t_val.place(relx=0.4, rely=0.6, relheight=0.1, relwidth=0.55)
 t_val.config(font=("Helvetica", 32))
 
-
+#Errors
+error = tk.Label(mainframe, text="ERROR: Invalid Input. Enter only a number or leave blank", fg=ImperialRed, bg=PrussianBlue)
+inputerror = tk.Label(mainframe, text="ERROR: Invalid Input. Enter at least 3 variables", fg=ImperialRed, bg=PrussianBlue)
 '''
    _________    __    ________  ____    ___  __________  ____
   / ____/   |  / /   / ____/ / / / /   /   |/_  __/ __ \/ __ \
@@ -198,101 +211,124 @@ t_val.config(font=("Helvetica", 32))
 
 '''
 def calculate():
-	global s
-	global u
-	global v
-	global a
-	global t
+        global s
+        global u
+        global v
+        global a
+        global t
 
-	s = s_value()
-	u = u_value()
-	v = v_value()
-	a = a_value()
-	t = t_value()
+        s = s_value()
+        u = u_value()
+        v = v_value()
+        a = a_value()
+        t = t_value()
 
-	if s == None and u == None:
-	    u = v - a*t
-	    u_val.insert(0, '%.3f' % u)
+        if s == None and u == None and v == None:
+            return InputErrorMessage()
+        elif s == None and u == None and a == None:
+            return InputErrorMessage()
+        elif s == None and u == None and t == None:
+            return InputErrorMessage()
+        elif s == None and v == None and a == None:
+            return InputErrorMessage()
+        elif s == None and v == None and t == None:
+            return InputErrorMessage()
+        elif s == None and a == None and t == None:
+            return InputErrorMessage()
+        elif u == None and v == None and a == None:
+            return InputErrorMessage()
+        elif u == None and v == None and t == None:
+            return InputErrorMessage()
+        elif u == None and a == None and t == None:
+            return InputErrorMessage()
+        elif v == None and a == None and t == None:
+            return InputErrorMessage()
 
-	elif s == None and v == None:
-	    v = u + a*t
-	    v_val.insert(0, '%.3f' % v)
 
-	elif s == None and a == None:
-	    a = (v-u)/t
-	    a_val.insert(0, '%.3f' % a)
+        if s == None and u == None:
+            u = v - a*t
+            u_val.insert(0, '%.3f' % u)
 
-	elif s == None and t == None:
-	    t = (v-u)/a
-	    t_val.insert(0, '%.3f' % t)
+        elif s == None and v == None:
+            v = u + a*t
+            v_val.insert(0, '%.3f' % v)
 
-	if s == None:
-	    s=(u+v)*t*0.5
-	    s_val.insert(0, '%.3f' % s)
+        elif s == None and a == None:
+            a = (v-u)/t
+            a_val.insert(0, '%.3f' % a)
+
+        elif s == None and t == None:
+            t = (v-u)/a
+            t_val.insert(0, '%.3f' % t)
+
+        if s == None:
+            s=(u+v)*t*0.5
+            s_val.insert(0, '%.3f' % s)
 
 
 	#When v == None
-	if v == None and u == None:
-	    u=(s/t)-0.5*a*t
-	    u_val.insert(0, '%.3f' % u)
+        if v == None and u == None:
+            u=(s/t)-0.5*a*t
+            u_val.insert(0, '%.3f' % u)
 
-	elif v == None and a == None:
-	    a = (2*(s-t*u))/t**2
-	    a_val.insert(0, '%.3f' % a)
+        elif v == None and a == None:
+            a = (2*(s-t*u))/t**2
+            a_val.insert(0, '%.3f' % a)
 
-	elif v == None and t == None:
-	    try:
-	        t = (math.sqrt(2*a*s+u**2)-u)/a                                    #Checks whether the calculation is possible
-	        t_val.insert(0, '%.3f' % t)                                        #If calculation is possible, the value is printed
-	    except ValueError:                                                     #If calculation is impossible, error message is printed
-	        t_val.insert(0, "")
+        elif v == None and t == None:
+            try:
+                t = (math.sqrt(2*a*s+u**2)-u)/a             #Checks whether the calculation is possible
+                t_val.insert(0, '%.3f' % t)                 #If calculation is possible, the value is printed
+            except ValueError:                              #If calculation is impossible, error message is printed
+                t_val.insert(0, "")
 
-	if v == None:
-	    try:                                                                   #Checks whether the calculation is possible
-	        v = u+a*t
-	        v_val.insert(0, '%.3f' % v)                                        #If calculation is possible, the value is printed
-	    except TypeError:
-	        v_val.insert(0, "No real roots for v")                             #If calculation is impossible, error message is printed
+        if v == None:
+            try:                                            #Checks whether the calculation is possible
+                v = u+a*t
+                v_val.insert(0, '%.3f' % v)                 #If calculation is possible, the value is printed
+            except TypeError:
+                v_val.insert(0, "No real roots for v")      #If calculation is impossible, error message is printed
 
 
 	#When a == None
-	if a == None and u == None:
-	    u = ((2*s)/t)-v
-	    u_val.insert(0, '%.3f' % u)
+        if a == None and u == None:
+            u = ((2*s)/t)-v
+            u_val.insert(0, '%.3f' % u)
 
-	elif a == None and t == None:
-	    t = (2*s)/(u+v)
-	    t_val.insert(0, '%.3f' % t)
+        elif a == None and t == None:
+            t = (2*s)/(u+v)
+            t_val.insert(0, '%.3f' % t)
 
-	if a == None:
-	    a = (v-u)/t
-	    a_val.insert(0, '%.3f' % a)
+        if a == None:
+            a = (v-u)/t
+            a_val.insert(0, '%.3f' % a)
 
 	#When t == None
-	if t == None and u == None:
-	    try:
-	        u = math.sqrt((v**2)-(2*a*s))                                      #Checks whether the calculation is possible
-	        u_val.insert(0, '%.3f' % u)                                        #If calculation is possible, the value is printed
-	    except ValueError:
-	        u_val.insert(0, "No real roots for u")                             #If calculation is impossible, error message is printed
+        if t == None and u == None:
+            try:
+                u = math.sqrt((v**2)-(2*a*s))               #Checks whether the calculation is possible
+                u_val.insert(0, '%.3f' % u)                 #If calculation is possible, the value is printed
+            except ValueError:
+                u_val.insert(0, "No real roots for u")      #If calculation is impossible, error message is printed
 
 
-	if t == None:
-	    try:                                                                   #Checks whether the calculation is possible
-	        t = (v-u)/a                                                        #If calculation is possible, the value is printed
-	        t_val.insert(0, '%.3f' % t)
-	    except TypeError:
-	        t_val.insert(0, "Can't solve for t")                               #If calculation is impossible, error message is printed
+        if t == None:
+            try:                                            #Checks whether the calculation is possible
+                t = (v-u)/a                                 #If calculation is possible, the value is printed
+                t_val.insert(0, '%.3f' % t)
+            except TypeError:
+                t_val.insert(0, "Can't solve for t")        #If calculation is impossible, error message is printed
+
 
 
 
 #Button to calculate values
-goButton = tk.Button(mainframe, text = "GO!", fg=Honeydew, bg=PrussianBlue, command=lambda: calculate())    #<-- Go button runs calculate() function
+goButton = tk.Button(mainframe, text = "GO!", fg=Xiketic, bg=PrussianBlue, command=lambda: [errorclear(), calculate()])     #<-- Go button runs calculate() function
 goButton.place(relx=0.55, rely=0.8, relheight = 0.15, relwidth=0.25)
 goButton.config(font=("Helvetica", 32))
 
 #Button to clear values
-clearButton = tk.Button(mainframe, text = "Clear", fg=Honeydew, bg=PrussianBlue, command=lambda: clear())   #<-- Clear button runs clear() function
+clearButton = tk.Button(mainframe, text = "Clear", fg=Xiketic, bg=PrussianBlue, command=lambda: [errorclear(), clear()])  #<-- Clear button runs clear() function
 clearButton.place(relx=0.2, rely=0.8, relheight = 0.15, relwidth=0.25)
 clearButton.config(font=("Helvetica", 32))
 
